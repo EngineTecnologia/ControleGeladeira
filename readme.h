34f444b33ea05cfc7e2b1e34404c2f6a56fc65fa
EngineTecnologia <69589794+EngineTecnologia@users.noreply.github.com>
2020-08-12 14:28:38 -0300

    Update readme.h

--- readme.h
+++ readme.h
@@ -1,5 +1,5 @@
 /*  Dispositivo IoT
- *  Autor: Raul Melo
+ *  Autor: Fernando Fasti
  *  Criado: 20/12/19
  *  Versão: 1.5.3
  *  Obs: versão inicialmente preparada para trasnmissão de dados via REST teve incluída uma rotina para transmissão para o thingspeak
